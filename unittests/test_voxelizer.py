dd3548a569792bd0dc03732bea5b7c85e399f446
p-hofmann <peter.hofmann@hhu.de>
2017-07-28 20:49:19 +0200

    modified test

--- unittests/test_voxelizer.py
+++ unittests/test_voxelizer.py
@@ -1,14 +1,20 @@
 __author__ = 'Peter Hofmann'
 
 import unittest
+import os
 from voxlib.voxelize import voxelize
 
 
 class PerimeterTest(unittest.TestCase):
-    input_ascii = "./cube_corner.stl"
-    input_binary = "./Dragon_2.5.stl"
+    input_file_paths = [
+        "./input/cube_corner.stl",
+        # "./input/Dragon_2.5.stl",
+        # "./input/Scaffold.obj"
+    ]
 
     def test_voxelize(self):
-        triangles = set(voxelize(self.input_ascii, 8))
-        self.assertGreaterEqual(len(triangles), 2)
+        for file_path in self.input_file_paths:
+            print(os.path.basename(file_path))
+            triangles = set(voxelize(file_path, 10))
+            self.assertGreaterEqual(len(triangles), 2)
         # print(x, y, z)
