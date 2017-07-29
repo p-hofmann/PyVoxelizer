f1b698227e5459be058da6eda7acb3fd83476120
p-hofmann <peter.hofmann@hhu.de>
2017-07-30 00:49:16 +0200

    big performance boost, but with minor issue

--- voxlib/voxelize.py
+++ voxlib/voxelize.py
@@ -82,6 +82,9 @@ def get_neighbours(position):
                 yield position_tmp
 
 
+range_p = [-1, 0, 1]
+
+
 def get_intersecting_voxels_depth_first(vertex_1, vertex_2, vertex_3):
     """
 
@@ -89,10 +92,12 @@ def get_intersecting_voxels_depth_first(vertex_1, vertex_2, vertex_3):
     @type vertex_2: numpy.ndarray
     @type vertex_3: numpy.ndarray
 
-    @rtype:
+    @rtype: list[(int, int, int)]
     """
+    result_positions = []
+    range_p = [-1, 0, 1]
     tmp_triangle = None
-    seed = int(round(vertex_1[0] - 0.5)), int(round(vertex_1[1] - 0.5)), int(round(vertex_1[2] - 0.5))
+    seed = int(vertex_1[0]), int(vertex_1[1]), int(vertex_1[2])
     searched = set()
     stack = set()
     stack.add(seed)
@@ -109,6 +114,8 @@ def get_intersecting_voxels_depth_first(vertex_1, vertex_2, vertex_3):
         tmp[0] = 0.5 + position[0]
         tmp[1] = 0.5 + position[1]
         tmp[2] = 0.5 + position[2]
+
+        # move raster to origin, test assumed triangle in relation to origin
         np.subtract(vertex_1, tmp, tmp_vertex_1)
         np.subtract(vertex_2, tmp, tmp_vertex_2)
         np.subtract(vertex_3, tmp, tmp_vertex_3)
@@ -118,12 +125,94 @@ def get_intersecting_voxels_depth_first(vertex_1, vertex_2, vertex_3):
         else:
             is_inside = t_c_intersection(tmp_triangle) == INSIDE
         if is_inside:
-            neighbours = list(get_neighbours(position))
+            result_positions.append(position)
+            # neighbours = list(get_neighbours(position))
+            for x in range_p:
+                for y in range_p:
+                    for z in range_p:
+                        neighbour = (position[0] + x, position[1] + y, position[2] + z)
+                        if neighbour not in searched:
+                            stack.add(neighbour)
+    del searched, stack
+    return result_positions
+
+
+def get_intersecting_voxels_depth_first_mod(vertex_1, vertex_2, vertex_3):
+    """
+
+    @type vertex_1: numpy.ndarray
+    @type vertex_2: numpy.ndarray
+    @type vertex_3: numpy.ndarray
+
+    @rtype: list[(int, int, int)]
+    """
+    result_positions = []
+    tmp_triangle = None
+    searched = set()
+    stack = set()
+
+    seed = int(vertex_1[0]), int(vertex_1[1]), int(vertex_1[2])
+    stack.add(seed)
+
+    tmp = np.array([0.0, 0.0, 0.0])
+    tmp_vertex_1 = np.array([0.0, 0.0, 0.0])
+    tmp_vertex_2 = np.array([0.0, 0.0, 0.0])
+    tmp_vertex_3 = np.array([0.0, 0.0, 0.0])
+    if not triangle_lib:
+        tmp_triangle = Triangle()
+        tmp_triangle.set(tmp_vertex_1, tmp_vertex_2, tmp_vertex_3)
+    while len(stack) > 0:
+        position = stack.pop()
+        searched.add(position)
+        tmp[0] = 0.5 + position[0]
+        tmp[1] = 0.5 + position[1]
+        tmp[2] = 0.5 + position[2]
+
+        # move raster to origin, test assumed triangle in relation to origin
+        np.subtract(vertex_1, tmp, tmp_vertex_1)
+        np.subtract(vertex_2, tmp, tmp_vertex_2)
+        np.subtract(vertex_3, tmp, tmp_vertex_3)
+        if triangle_lib:
+            is_inside = triangle_lib.t_c_intersection(
+                vertexes_to_c_triangle(tmp_vertex_1, tmp_vertex_2, tmp_vertex_3)) == INSIDE
+        else:
+            is_inside = t_c_intersection(tmp_triangle) == INSIDE
+        if is_inside:
+            result_positions.append(position)
+
+            neighbours = set()
+            if position[0] > vertex_2[0]:
+                neighbours.add((position[0] - 1, position[1], position[2]))
+                if position[0] < vertex_3[0]:
+                    neighbours.add((position[0] + 1, position[1], position[2]))
+            else:
+                neighbours.add((position[0] + 1, position[1], position[2]))
+                if position[0] > vertex_3[0]:
+                    neighbours.add((position[0] - 1, position[1], position[2]))
+
+            if position[1] > vertex_2[1]:
+                neighbours.add((position[0], position[1] - 1, position[2]))
+                if position[1] < vertex_3[1]:
+                    neighbours.add((position[0], position[1] + 1, position[2]))
+            else:
+                neighbours.add((position[0], position[1] + 1, position[2]))
+                if position[1] > vertex_3[1]:
+                    neighbours.add((position[0], position[1] - 1, position[2]))
+
+            if position[2] > vertex_2[2]:
+                neighbours.add((position[0], position[1], position[2] - 1))
+                if position[2] < vertex_3[2]:
+                    neighbours.add((position[0], position[1], position[2] + 1))
+            else:
+                neighbours.add((position[0], position[1], position[2] + 1))
+                if position[2] > vertex_3[2]:
+                    neighbours.add((position[0], position[1], position[2] - 1))
+
             for neighbour in neighbours:
                 if neighbour not in searched:
                     stack.add(neighbour)
-            yield position
     del searched, stack
+    return result_positions
 
 
 def voxelize(file_path, resolution):
@@ -143,8 +232,7 @@ def voxelize(file_path, resolution):
 
         (vertex_1, vertex_2, vertex_3) = scale_and_shift_triangle(triangle, scale, shift)
         bounding_box.from_vertexes(vertex_1, vertex_2, vertex_3)
-        for (x, y, z) in get_intersecting_voxels_depth_first(vertex_1, vertex_2, vertex_3):
-            voxels.add((x, y, z))
+        voxels.update(get_intersecting_voxels_depth_first_mod(vertex_1, vertex_2, vertex_3))
     center = bounding_box.get_center()
     while len(voxels) > 0:
         (x, y, z) = voxels.pop()
