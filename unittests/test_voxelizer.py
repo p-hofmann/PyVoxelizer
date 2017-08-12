de49bdc3992502b6619cbdef80c6bfac23828d50
hofmann <priggelpit@web.de>
2017-08-12 21:13:13 +0200

    adding test for triangle causing floating point issue

--- unittests/test_voxelizer.py
+++ unittests/test_voxelizer.py
@@ -1,6 +1,6 @@
 import unittest
 import os
-from voxlib.voxelize import voxelize
+from voxlib.voxelize import voxelize, get_intersecting_voxels_depth_first, scale_and_shift_triangle
 
 
 class PerimeterTest(unittest.TestCase):
@@ -35,6 +35,20 @@ class PerimeterTest(unittest.TestCase):
         voxels = set(voxelize(file_path, resolution))
         print(len(voxels))
         self.assertEqual(len(voxels), 730)
+
+    def test_get_intersecting_voxels_depth_first(self):
+        scale = 0.2171953325381205
+        shift = [103.419, 65.4, 68.2169]
+        triangle = ((-39.3653, 8.43406, 86.9206), (-66.9773, -5.08748, 86.9206), (-39.3653, -8.43406, 86.9206))
+        (vertex_1, vertex_2, vertex_3) = scale_and_shift_triangle(triangle, scale, shift)
+        new_set = get_intersecting_voxels_depth_first(vertex_1, vertex_2, vertex_3)
+        center = (
+            int((vertex_1[0] + vertex_2[0] + vertex_3[0]) / 3.),
+            int((vertex_1[1] + vertex_2[1] + vertex_3[1]) / 3.),
+            int((vertex_1[2] + vertex_2[2] + vertex_3[2]) / 3.)
+            )
+        self.assertTrue(center in new_set, center)
+
 
 if __name__ == '__main__':
     unittest.main()
