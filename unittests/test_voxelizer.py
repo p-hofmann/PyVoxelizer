43627bac3a05ec1add5149076205cea9025cc7fc
hofmann <priggelpit@web.de>
2017-08-17 22:36:35 +0200

    updated test

--- unittests/test_voxelizer.py
+++ unittests/test_voxelizer.py
@@ -15,32 +15,20 @@ class PerimeterTest(unittest.TestCase):
 
     def test_obj_texture(self):
         rgba_color_map_armor = {
-            None                   :  63,  # glass
-            (0   , 0   , 0   , 1.0): 603,  # black
-            (0.9 , 0.9 , 0.9 , 1.0): 608,  # white
-            (0.5 , 0   , 0.5 , 1.0): 613,  # purple
-            (0   , 0.1 , 0.75, 1.0): 618,  # blue
-            (0   , 0.75, 0   , 1.0): 623,  # green
-            (0.9 , 0.9 , 0   , 1.0): 628,  # yellow
-            (0.9 , 0.5 , 0   , 1.0): 633,  # orange
-            (0.75, 0   , 0   , 1.0): 638,  # red
-            (0.9 , 0.75, 0.5 , 1.0): 643,  # brown
-            (0   , 0.6 , 0.6 , 1.0): 878,  # teal
-            (0.9 , 0   , 0.9 , 1.0): 912,  # pink / fuchsia
+            (  0,   0,   0): {255: 603, 127: 593, 0:  63},  # black
+            (255, 255, 255): {255: 608, 127: 507},  # white
+            (127,   0, 127): {255: 613, 127: 537},  # purple
+            (  0,   0, 192): {255: 618, 127: 532},  # blue
+            (  0, 192,   0): {255: 623, 127: 527},  # green
+            (255, 255,   0): {255: 628, 127: 522},  # yellow
+            (255, 127,   0): {255: 633, 127: 517},  # orange
+            (192,   0,   0): {255: 638, 127: 512},  # red
+            (255, 192, 127): {255: 643, 127: 690},  # brown
+            (  0, 127, 127): {255: 878, 127: 883},  # teal
+            (255,   0, 255): {255: 912, 127: 917},  # pink / fuchsia
 
-            (0   , 0   , 0   , 0.75): 593,  # black
-            (0.25, 0.25, 0.25, 0.75): 828,  # dark grey
-            (0.5 , 0.5 , 0.5 , 0.5): 598,  # grey
-            (1.0 , 1.0 , 1.0 , 0.5): 507,  # white
-            (0.5 , 0   , 0.5 , 0.5): 537,  # purple
-            (0   , 0   , 0.5 , 0.5): 532,  # blue
-            (0   , 0.5 , 0   , 0.5): 527,  # green
-            (1.0 , 1.0 , 0   , 0.5): 522,  # yellow
-            (1.0 , 0.5 , 0   , 0.5): 517,  # orange
-            (0.5 , 0   , 0   , 0.5): 512,  # red
-            (1.0 , 0.75, 0.5 , 0.5): 690,  # brown
-            (0   , 0.5 , 0.5 , 0.5): 883,  # teal
-            (1.0 , 0   , 1.0 , 0.5): 917,  # pink / fuchsia
+            ( 64,  64,  64): {255: 828, 127: 828},  # dark grey
+            (127, 127, 127): {255: 598, 127: 598},  # grey
             }
         file_path = self.input_file_paths[3]
         print(os.path.basename(file_path))
@@ -76,11 +64,11 @@ class PerimeterTest(unittest.TestCase):
         print(len(voxels))
         self.assertEqual(len(voxels), 730)
 
-    def test_test(self):
-        file_path = "./input/vaygr-mobile-refinery.zip"
-        print(os.path.basename(file_path))
-        resolution = 512
-        voxels = set(list(Voxelizer.voxelize(file_path, resolution)))
+    # def test_test(self):
+    #     file_path = "./input/vaygr-mobile-refinery.zip"
+    #     print(os.path.basename(file_path))
+    #     resolution = 512
+    #     voxels = set(list(Voxelizer.voxelize(file_path, resolution)))
 
     def test_get_intersecting_voxels_depth_first(self):
         scale = 0.2171953325381205
